4da468b7e87b5cba45d34423fc817cf7f6b93a11
Ankur Jaiswal <ankurjaiswalofficial@gmail.com>
2024-08-26 19:56:26 +0000

    fullscreen handler fixed

--- src/app/meet/components/ActionFooter/MeetingHandlers/MoreOptionsHandler/FullscreenHandler.tsx
+++ src/app/meet/components/ActionFooter/MeetingHandlers/MoreOptionsHandler/FullscreenHandler.tsx
@@ -6,7 +6,7 @@ import { toggleFullscreen } from '@/app/redux/slices/fullscreenSlice';
 import { RootState } from '@/app/redux/store';
 import { useAppDispatch } from '@/app/redux/hooks';
 
-export default function FullscreenHandler(): JSX.Element {
+export default function FullscreenHandler() {
     const isFullscreen = useSelector((state: RootState) => state.fullScreen.isFullscreen);
 
     const dispatch = useAppDispatch();
@@ -16,7 +16,7 @@ export default function FullscreenHandler(): JSX.Element {
 
     return (
         <DropdownMenuItem className={"h-12"} onClick={handleIsFullscreen}>
-            <div className="w-full flex flex-row items-center justify-start" >
+            <div className="w-full flex flex-row items-center justify-start">
                 <FullscreenIcon className="w-6 h-6 mr-2" />
                 <div className="flex flex-col items-start justify-center">
                     {isFullscreen ? <p>Exit Full Screen</p> : <p>Full Screen</p>}
